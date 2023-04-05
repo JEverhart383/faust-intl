1c6124a2abcea09a8e5846ca7197f69879024daa
Jeff Everhart <Jeffeverhart383@gmail.com>
2023-04-05 17:43:06 -0400

    update env var

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   env : {
-    NEXT_PUBLIC_WORDPRESS_URL: 'https://faustintl.wpengine.com/'
+    NEXT_PUBLIC_WORDPRESS_URL: 'https://faustintl.wpengine.com/',
   },
   e2e: {
     baseUrl: 'http://localhost:3000',
